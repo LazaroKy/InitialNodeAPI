4b87dd810916e7a4db756ccf7ab897e2c4181ca3
LázaroK <lazarolk23@hotmail.com>
2025-02-23 15:59:04 -0300

    (Feat) Created class Product for product management

---
+++ ProductsAPI/model/product.js
@@ -0,0 +1,31 @@
+const products = []; // Array to simulate database
+
+class Product {
+    static listAllProducts(){
+        return products;
+    }
+
+    static getProductById(id){
+        return products.find(product=> product.id = id);
+    }
+
+    static createProduct(product){
+        product.id = product.length + 1; //Id unique
+        products.push(product);
+    }
+
+    static updateProduct(id, updateDatas){
+        const index = products.findIndex(product=> product.id === id);
+        if(index !== -1){
+            products[index] = {...products[index], ...updateDatas} //mirroring operator - overwriting properties of the product chosen
+        }
+    }
+
+    static deleteProduct(id){
+        const index = products.findIndex(product=> product.id === id);
+        if(index !== -1){
+            products.slice(index, 1) // "slice" - doesn't change the source array 
+        }
+    }
+}
+module.exports= Product; //export file as "Product" to be able to request
